6984763db1e43181e9d5c2ab4724c9c37d75569a
kalucky0 <kalucki23@gmail.com>
2022-03-08 14:58:58 +0100

    Create Main function

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace po_lab
+{
+    public class Program {
+        public static void Main(string[] args) {
+            PersonProperties person = new PersonProperties();
+            person.FirstName = "Jan";
+            Console.WriteLine(person.FirstName);
+
+            Money money = Money.Of(100, Currency.PLN)!;
+            Money money2 = Money.Of(100, Currency.PLN)!;
+
+            Console.WriteLine(money == money2);
+            Console.WriteLine(money + money2);
+            Console.WriteLine(money - money2);
+            Console.WriteLine(money * 2);
+            Console.WriteLine(2 * money);
+            Console.WriteLine(money / 2);
+            Console.WriteLine(2 / money);
+        }
+    }
+}
