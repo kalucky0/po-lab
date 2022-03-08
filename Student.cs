08315f82fac22d5eb266fd1a8a900223afccd756
kalucky0 <kalucki23@gmail.com>
2022-03-08 15:35:58 +0100

    Add IComparable to Student class

--- Student.cs
+++ Student.cs
@@ -1,6 +1,6 @@
 namespace po_lab
 {
-    public class Student {
+    public class Student : IComparable<Student> {
         public string Surname { get; set; }
         public string Name { get; set; }
         public decimal Average { get; set; }
@@ -9,6 +9,17 @@ namespace po_lab
             Surname = surname;
             Name = name;
             Average = average;
+        }
+        
+        public int CompareTo(Student? other)
+        {
+            if (ReferenceEquals(null, other)) return 1;
+            if (ReferenceEquals(this, other)) return 0;
+            var surnameComparison = Surname.CompareTo(other.Surname);
+            if (surnameComparison != 0) return surnameComparison;
+            var nameComparison = Name.CompareTo(other.Name);
+            if (nameComparison != 0) return nameComparison;
+            return Average.CompareTo(other.Average);
         }
     }
 }
