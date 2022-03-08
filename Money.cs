635f73b0b5bf05904352fb4dcff8816f836ab82f
kalucky0 <kalucki23@gmail.com>
2022-03-08 13:38:02 +0100

    Initial commit

---
+++ Money.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace po_lab
+{
+    public class Money : IEquatable<Money>
+    {
+        private readonly decimal _value;
+        private readonly Currency _currency;
+        private Money(decimal value, Currency currency)
+        {
+            _value = value;
+            _currency = currency;
+        }
+
+        public bool Equals(Money? other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            return false;
+        }
+
+        public static Money? Of(decimal value, Currency currency)
+        {
+            return value < 0 ? null : new Money(value, currency);
+        }
+
+        public static Money OfWithException(decimal value, Currency currency)
+        {
+            if (value < 0)
+                throw new ArgumentException("Value cannot be below zero");
+
+            return new Money(value, currency);
+        }
+
+        public static Money ParseValue(string valueStr, Currency currency)
+        {
+            var value = decimal.Parse(valueStr);
+            return new Money(value, currency);
+        }
+    }
+
+
+    public enum Currency
+    {
+        PLN = 1,
+        USD = 2,
+        EUR = 3
+    }
+}
---
+++ PersonProperties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace po_lab
+{
+    public class PersonProperties
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    firstName = value;
+                }
+            }
+        }
+    }
+}
