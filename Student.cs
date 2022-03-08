c9e0143e13354005a082865c47c1a9edcdf33d92
kalucky0 <kalucki23@gmail.com>
2022-03-08 15:45:00 +0100

    Add documentation to Student class

--- Student.cs
+++ Student.cs
@@ -5,12 +5,22 @@ namespace po_lab
         public string Name { get; set; }
         public decimal Average { get; set; }
 
+        /// <summary>
+        /// Constructor of Student class
+        /// </summary>
+        /// <param name="surname">Surname of student</param>
+        /// <param name="name">Name of student</param>
+        /// <param name="average">Average of student</param>
         public Student(string surname, string name, decimal average) {
             Surname = surname;
             Name = name;
             Average = average;
         }
         
+        /// <summary>
+        /// Method that compares two students
+        /// </summary>
+        /// <param name="other">Student to compare with</param>
         public int CompareTo(Student? other)
         {
             if (ReferenceEquals(null, other)) return 1;
