6b2579f5cfbf73190dbf43ba4f6a1abc22338736
kalucky0 <kalucki23@gmail.com>
2022-03-15 15:18:14 +0100

    Create Hydroplane class

---
+++ po-lab2/Hydroplane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace po_lab2
+{
+    public class Hydroplane : Vehicle, Flyable, Swimmingable
+    {
+        public override decimal Drive(int distance)
+        {
+            Console.WriteLine("DRIVE");
+            return 0;
+        }
+        public decimal Swim(int distance)
+        {
+            Console.WriteLine("SWIM");
+            return 0;
+        }
+        public bool TakeOff()
+        {
+            Console.WriteLine("TAKE OFF");
+            return true;
+        }
+        public decimal Fly(int distance)
+        {
+            Console.WriteLine("FLY");
+            return 0;
+        }
+        public bool Land()
+        {
+            Console.WriteLine("LAND");
+            return true;
+        }
+    }
+}
