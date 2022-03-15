97183123be8d855c40b2a9e7d0aae03d10e15924
kalucky0 <kalucki23@gmail.com>
2022-03-15 15:17:53 +0100

    Create Truck class

---
+++ po-lab2/Truck.cs
@@ -0,0 +1,10 @@
+﻿namespace po_lab2
+{
+    public class Truck : Vehicle, Driveable
+    {
+        public override decimal Drive(int distance)
+        {
+            return 0;
+        }
+    }
+}
