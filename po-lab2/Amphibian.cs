941b773e51b71364d2090bf8c8d640e7e8fc3892
kalucky0 <kalucki23@gmail.com>
2022-03-15 15:18:04 +0100

    Create Amphibian class

---
+++ po-lab2/Amphibian.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace po_lab2
+{
+    public class Amphibian : Vehicle, Driveable, Swimmingable
+    {
+        public override decimal Drive(int distance)
+        {
+            Console.WriteLine("DRIVE");
+            return 0;
+        }
+        public decimal Swim(int distance)
+        {
+            Console.WriteLine("SWIM");
+            return 0;
+        }
+    }
+}
