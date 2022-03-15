4f6ed1b58e091c5a034c40033a2f4d78b0118d09
kalucky0 <kalucki23@gmail.com>
2022-03-15 15:16:21 +0100

    Create Bicycle and Car classes

---
+++ po-lab2/Bicycle.cs
@@ -0,0 +1,21 @@
+﻿namespace po_lab2
+{
+    public class Bicycle : Vehicle
+    {
+        public bool isDriver { get; set; }
+        public override decimal Drive(int distance)
+        {
+            if (isDriver)
+            {
+                _mileage += distance;
+                return (decimal)(distance / (double)MaxSpeed);
+            }
+            return -1;
+        }
+        public override string ToString()
+        {
+            return $"Bicycle{{Weight: {Weight}, MaxSpeed: {MaxSpeed}, Mileage: {_mileage}}}"; ;
+        }
+    }
+}
+
---
+++ po-lab2/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace po_lab2
+{
+    public class Car : Vehicle
+    {
+        public bool isFuel { get; set; }
+        public bool isEngineWorking { get; set; }
+        public override decimal Drive(int distance)
+        {
+            if (isFuel && isEngineWorking)
+            {
+                _mileage += distance;
+                return (decimal)(distance / (double)MaxSpeed);
+            }
+            return -1;
+        }
+        public override string ToString()
+        {
+            return $"Car{{Weight: {Weight}, MaxSpeed: {MaxSpeed}, Mileage: {_mileage}}}";
+        }
+    }
+}
