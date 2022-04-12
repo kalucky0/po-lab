128d94cac77fda94f4ed5d2c7b21fb97068140e6
kalucky0 <kalucki23@gmail.com>
2022-04-12 14:28:47 +0200

    Create po_lab6

---
+++ po-lab6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace po_lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
